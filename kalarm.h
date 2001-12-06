551cdad500400a634a2dc823a5420261751aae97
David Jarvie <djarvie@kde.org>
2001-12-06 08:59:56 +0000

    Update version number
    svn path=/trunk/kdepim/; revision=125778

--- kalarm.h
+++ kalarm.h
@@ -1,21 +1,13 @@
 /*
  *  kalarm.h  -  global header file
  *  Program:  kalarm
+ *
  *  (C) 2001 by David Jarvie  software@astrojar.org.uk
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #ifndef KALARM_H
@@ -26,7 +18,10 @@
 #include <config.h>
 #endif
 
+#undef VERSION
 #define VERSION "0.3.5"
+
+#define KALARM_DCOP_OBJECT "display"
 
 
 #endif // KALARM_H
