f00a3ea58d002a48f3bf2e498712f4d9b8d2155e
David Jarvie <djarvie@kde.org>
2001-12-17 08:40:29 +0000

    Update licence comments in header
    svn path=/trunk/kdepim/; revision=127528

--- prefsettings.h
+++ prefsettings.h
@@ -4,10 +4,19 @@
  *
  *  (C) 2001 by David Jarvie  software@astrojar.org.uk
  *
- *  This program is free software; you can redistribute it and/or modify  *
- *  it under the terms of the GNU General Public License as published by  *
- *  the Free Software Foundation; either version 2 of the License, or     *
- *  (at your option) any later version.                                   *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 #ifndef PREFSETTINGS_H
@@ -41,7 +50,7 @@ class GeneralSettings : public SettingsBase
 public:
 	GeneralSettings(QWidget* parent);
 	~GeneralSettings();
-	
+
 	QColor defaultBgColour() const   { return m_defaultBgColour; }
 	const QFont&  messageFont() const   { return m_messageFont; }
 	// some virtual functions that will be overloaded from the base class
