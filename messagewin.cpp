3bb415ae0070472bb6d74ffe95c8bcd7d4432089
David Jarvie <djarvie@kde.org>
2002-02-19 23:20:30 +0000

    Modify Close button creation
    svn path=/trunk/kdepim/; revision=137529

--- messagewin.cpp
+++ messagewin.cpp
@@ -215,7 +215,7 @@ QSize MessageWin::initView()
 	grid->setColStretch(0, 1);     // keep the buttons right-adjusted in the window
 
 	// Close button
-	QPushButton* okButton = new KPushButton(KStdGuiItem::close(), topWidget);
+	QPushButton* okButton = new QPushButton(KStdGuiItem::close().text(), topWidget);
 	connect(okButton, SIGNAL(clicked()), SLOT(close()));
 	grid->addWidget(okButton, 0, 1, AlignHCenter);
 	QWhatsThis::add(okButton, i18n("Acknowledge the alarm"));
