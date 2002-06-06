13114cab9437d358c8c2e5636cdd7b2df2c603b1
David Jarvie <djarvie@kde.org>
2002-06-06 17:19:01 +0000

    Alarm deferral dialog
    svn path=/trunk/kdepim/; revision=159641

---
+++ deferdlg.cpp
@@ -0,0 +1,90 @@
+/*
+ *  deferdlg.cpp  -  dialogue to defer an alarm
+ *  Program:  kalarm
+ *  (C) 2002 by David Jarvie  software@astrojar.org.uk
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "kalarm.h"
+
+#include <qlayout.h>
+
+#include <kglobal.h>
+#include <klocale.h>
+#include <kmessagebox.h>
+
+#include "datetime.h"
+#include "deferdlg.moc"
+
+
+DeferAlarmDlg::DeferAlarmDlg(const QString& caption, const QDateTime& initialDT, const QDateTime& limitDT,
+                             bool cancelButton, QWidget* parent, const char* name)
+	: KDialogBase(parent, name, true, caption, Ok|Cancel|User1, Ok, false, i18n("Cancel &Deferral")),
+	  limitDateTime(limitDT)
+{
+	if (!cancelButton)
+		showButton(User1, false);
+
+	QWidget* page = new QWidget(this);
+	setMainWidget(page);
+	QVBoxLayout* layout = new QVBoxLayout(page, 0, spacingHint());
+
+	timeWidget = new AlarmTimeWidget(AlarmTimeWidget::DEFER_TIME, 0, page, "timeGroup");
+	timeWidget->setDateTime(initialDT);
+	layout->addWidget(timeWidget);
+	layout->addSpacing(KDialog::spacingHint());
+
+	setButtonWhatsThis(User1, i18n("Cancel the deferred alarm. This does not affect future recurrences."));
+}
+
+
+DeferAlarmDlg::~DeferAlarmDlg()
+{
+}
+
+
+/******************************************************************************
+*  Called when the OK button is clicked.
+*/
+void DeferAlarmDlg::slotOk()
+{
+	bool anyTime;
+	if (timeWidget->getDateTime(alarmDateTime, anyTime))
+	{
+		if (alarmDateTime >= limitDateTime)
+			KMessageBox::sorry(this, i18n("Cannot defer past the alarm's next recurrence (currently %1)")
+				                          .arg(KGlobal::locale()->formatDateTime(limitDateTime)));
+		else
+			accept();
+	}
+}
+
+/******************************************************************************
+*  Called when the Cancel Deferral button is clicked.
+*/
+void DeferAlarmDlg::slotUser1()
+{
+	alarmDateTime = QDateTime();
+	accept();
+}
+
+/******************************************************************************
+*  Called when the Cancel button is clicked.
+*/
+void DeferAlarmDlg::slotCancel()
+{
+	reject();
+}
---
+++ deferdlg.h
@@ -0,0 +1,51 @@
+/*
+ *  deferdlg.h  -  dialogue to defer an alarm
+ *  Program:  kalarm
+ *  (C) 2002 by David Jarvie  software@astrojar.org.uk
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef DEFERDLG_H
+#define DEFERDLG_H
+
+#include <qdatetime.h>
+#include <kdialogbase.h>
+
+class AlarmTimeWidget;
+
+
+class DeferAlarmDlg : public KDialogBase
+{
+		Q_OBJECT
+	public:
+		DeferAlarmDlg(const QString& caption, const QDateTime& initialDT, const QDateTime& limitDT,
+		              bool cancelButton, QWidget* parent = 0, const char* name = 0);
+		virtual ~DeferAlarmDlg();
+
+		const QDateTime&  getDateTime() const  { return alarmDateTime; }
+
+	protected slots:
+		virtual void slotOk();
+		virtual void slotCancel();
+		virtual void slotUser1();
+
+	private:
+		AlarmTimeWidget* timeWidget;
+		QDateTime        limitDateTime;
+		QDateTime        alarmDateTime;
+};
+
+#endif // DEFERDLG_H
