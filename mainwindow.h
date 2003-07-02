e3dfd74e784dca2242475f8fba2f1a40d3921fb4
David Jarvie <djarvie@kde.org>
2003-07-02 15:07:20 +0000

    Added time-to-alarm menu options. Added drag-and-drop onto window.
    svn path=/trunk/kdepim/; revision=235088

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,7 @@
 /*
  *  mainwindow.h  -  main application window
  *  Program:  kalarm
- *  (C) 2001 - 2003 by David Jarvie  software@astrojar.org.uk
+ *  (C) 2001, 2002, 2003 by David Jarvie  software@astrojar.org.uk
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
  *  As a special exception, permission is given to link this program
  *  with any edition of Qt, and distribute the resulting executable,
@@ -40,8 +40,8 @@ class KAlarmMainWindow : public MainWindowBase
 	public:
 		KAlarmMainWindow(bool restored = false);
 		~KAlarmMainWindow();
-		bool           trayParent() const;
-		bool           hiddenTrayParent() const     { return mHiddenTrayParent; }
+		bool           isTrayParent() const;
+		bool           isHiddenTrayParent() const   { return mHiddenTrayParent; }
 		bool           showingExpired() const       { return mShowExpired; }
 
 		void           selectEvent(const QString& eventID);
@@ -57,6 +57,9 @@ class KAlarmMainWindow : public MainWindowBase
 		static void    modifyEvent(const KAlarmEvent& event, KAlarmMainWindow* w)   { modifyEvent(event.id(), event, w); }
 		static void    deleteEvent(const QString& eventID, KAlarmMainWindow*);
 		static void    undeleteEvent(const QString& oldEventID, const KAlarmEvent& event, KAlarmMainWindow*);
+		static void    executeNew(KAlarmMainWindow* = 0, KAlarmEvent::Action = KAlarmEvent::MESSAGE, const QString& text = QString::null);
+		static void    executeDragEnterEvent(QDragEnterEvent*);
+		static void    executeDropEvent(KAlarmMainWindow*, QDropEvent*);
 		static void              closeAll();
 		static KAlarmMainWindow* toggleWindow(KAlarmMainWindow*);
 		static KAlarmMainWindow* mainMainWindow();
@@ -66,7 +69,10 @@ class KAlarmMainWindow : public MainWindowBase
 	protected:
 		virtual void   resizeEvent(QResizeEvent*);
 		virtual void   showEvent(QShowEvent*);
+		virtual void   hideEvent(QHideEvent*);
 		virtual void   closeEvent(QCloseEvent*);
+		virtual void   dragEnterEvent(QDragEnterEvent*);
+		virtual void   dropEvent(QDropEvent*);
 		virtual void   saveProperties(KConfig*);
 		virtual void   readProperties(KConfig*);
 
@@ -85,13 +91,19 @@ class KAlarmMainWindow : public MainWindowBase
 		void           slotSelection();
 		void           slotMouseClicked(int button, QListViewItem* item, const QPoint&, int);
 		void           slotDoubleClicked(QListViewItem*);
+		void           slotShowTime();
+		void           slotShowTimeTo();
 		void           slotShowExpired();
+		void           slotUpdateTimeTo();
 		void           updateTrayIconAction();
 		void           updateActionsMenu();
 		void           setAlarmEnabledStatus(bool status);
 
 	private:
+		void           createListView(bool recreate);
 		void           initActions();
+		static KAlarmEvent::Action  getDropAction(QDropEvent*, QString& text);
+		static void    setUpdateTimer();
 		static bool    findWindow(KAlarmMainWindow*);
 
 		static QPtrList<KAlarmMainWindow> windowList;  // active main windows
@@ -104,14 +116,22 @@ class KAlarmMainWindow : public MainWindowBase
 		KAction*       actionUndelete;
 		KAction*       actionToggleTrayIcon;
 		KAction*       actionRefreshAlarms;
+		KAction*       actionShowTime;
+		KAction*       actionShowTimeTo;
 		KAction*       actionShowExpired;
 		KAction*       actionQuit;
+		int            mShowTimeId;
+		int            mShowTimeToId;
 		int            mShowExpiredId;
 		int            mShowTrayId;
 		KPopupMenu*    mActionsMenu;
 		KPopupMenu*    mViewMenu;
 		int            mAlarmsEnabledId;     // alarms enabled item in Actions menu
+		QTimer*        mMinuteTimer;         // timer every minute
+		bool           mMinuteTimerSyncing;  // minute timer hasn't synchronised to minute boundary
 		bool           mHiddenTrayParent;    // on session restoration, hide this window
+		bool           mShowTime;            // show alarm times
+		bool           mShowTimeTo;          // show time-to-alarms
 		bool           mShowExpired;         // include expired alarms in the displayed list
 };
 
