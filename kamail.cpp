3e2e9a38ad78502b85fa2326663e4383b89282b6
David Jarvie <djarvie@kde.org>
2004-08-26 17:48:37 +0000

    Allow whitespace in mail attachment list
    svn path=/trunk/kdepim/; revision=341450

--- kamail.cpp
+++ kamail.cpp
@@ -16,16 +16,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *
- *  In addition, as a special exception, the copyright holders give permission
- *  to link the code of this program with any edition of the Qt library by
- *  Trolltech AS, Norway (or with modified versions of Qt that use the same
- *  license as Qt), and distribute linked combinations including the two.
- *  You must obey the GNU General Public License in all respects for all of
- *  the code used other than Qt.  If you modify this file, you may extend
- *  this exception to your version of the file, but you are not obligated to
- *  do so. If you do not wish to do so, delete this exception statement from
- *  your version.
  */
 
 #include "kalarm.h"
@@ -565,7 +555,6 @@ QString KAMail::convertAttachments(const QString& items, QStringList& list)
 {
 	KURL url;
 	list.clear();
-	QCString addrs = items.local8Bit();
 	int length = items.length();
 	for (int next = 0;  next < length;  )
 	{
@@ -578,7 +567,7 @@ QString KAMail::convertAttachments(const QString& items, QStringList& list)
 			sc = items.length();
 		if (sc < i)
 			i = sc;
-		QString item = items.mid(next, i - next);
+		QString item = items.mid(next, i - next).stripWhiteSpace();
 		switch (checkAttachment(item))
 		{
 			case 1:   list += item;  break;
