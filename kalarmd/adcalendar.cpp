3df7728beac60e02742284288692348135964497
David Jarvie <djarvie@kde.org>
2004-11-12 21:28:16 +0000

    Tailor kalarmd to KAlarm. Merge adcalendarbase.* into adcalendar.*
    svn path=/trunk/kdepim/; revision=362553

--- kalarmd/adcalendar.cpp
+++ kalarmd/adcalendar.cpp
@@ -1,106 +1,209 @@
 /*
-    Calendar access for KAlarm Alarm Daemon.
+ *  adcalendar.cpp  -  calendar file access
+ *  Program:  KAlarm's alarm daemon (kalarmd)
+ *  (C) 2001, 2004 by David Jarvie <software@astrojar.org.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
-    This file is part of the KAlarm alarm daemon.
-    Copyright (c) 2001, 2004 David Jarvie <software@astrojar.org.uk>
+#include <unistd.h>
+#include <assert.h>
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+#include <qfile.h>
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
-*/
-
+#include <ktempfile.h>
+#include <kio/job.h>
+#include <kio/jobclasses.h>
 #include <kdebug.h>
 
 #include "adcalendar.h"
 
+QValueList<ADCalendar*> ADCalendar::mCalendars;
+ADCalendar::EventsMap   ADCalendar::mEventsHandled;
+
+
 ADCalendar::ADCalendar(const QString& url, const QCString& appname)
-  : ADCalendarBase(url, appname),
-    available_( false ),
-    enabled_(true)
+	: mUrlString(url),
+	  mAppName(appname),
+	  mLoaded(false),
+	  mLoadedConnected(false),
+	  mUnregistered(false),
+	  mEnabled(true)
 {
-  loadFile();
+	ADCalendar* cal = getCalendar(url);
+	if (cal)
+	{
+		kdError(5900) << "ADCalendar::ADCalendar(" << url << "): calendar already exists" << endl;
+		assert(0);
+	}
+	loadFile(false);
+	mCalendars.append(this);
 }
 
-ADCalendar *ADCalendarFactory::create(const QString& url, const QCString& appname)
+ADCalendar::~ADCalendar()
 {
-  return new ADCalendar(url, appname);
+	clearEventsHandled();
+	mCalendars.remove(this);
 }
 
-/*
- * Check whether all the alarms for the event with the given ID have already
- * been handled.
- */
-bool ADCalendar::eventHandled(const Event* event, const QValueList<QDateTime>& alarmtimes)
+/******************************************************************************
+* Load the calendar file.
+*/
+bool ADCalendar::loadFile(bool reset)
 {
-  EventsMap::ConstIterator it = eventsHandled_.find(event->uid());
-  if (it == eventsHandled_.end())
-    return false;
-
-  int oldCount = it.data().alarmTimes.count();
-  int count = alarmtimes.count();
-  for (int i = 0;  i < count;  ++i) {
-    if (alarmtimes[i].isValid()) {
-      if (i >= oldCount                              // is it an additional alarm?
-      ||  !it.data().alarmTimes[i].isValid()         // or has it just become due?
-      ||  it.data().alarmTimes[i].isValid()          // or has it changed?
-       && alarmtimes[i] != it.data().alarmTimes[i])
-        return false;     // this alarm has changed
-    }
-  }
-  return true;
+	if (reset)
+		clearEventsHandled();
+	if (!mTempFileName.isNull())
+	{
+		// Don't try to load the file if already downloading it
+		kdError(5900) << "ADCalendar::loadFile(): already downloading another file\n";
+		return false;
+	}
+	mLoaded = false;
+	KURL url(mUrlString);
+	if (url.isLocalFile())
+	{
+		// It's a local file
+		loadLocalFile(url.path());
+		emit loaded(this, mLoaded);
+	}
+	else
+	{
+		// It's a remote file. Download to a temporary file before loading it
+		KTempFile tempFile;
+		mTempFileName = tempFile.name();
+		KURL dest;
+		dest.setPath(mTempFileName);
+		KIO::FileCopyJob* job = KIO::file_copy(url, dest, -1, true);
+		connect(job, SIGNAL(result(KIO::Job*)), SLOT(slotDownloadJobResult(KIO::Job*)));
+	}
+	return true;
 }
 
-/*
- * Remember that the specified alarms for the event with the given ID have been
- * handled.
- */
-void ADCalendar::setEventHandled(const Event* event, const QValueList<QDateTime>& alarmtimes)
+void ADCalendar::slotDownloadJobResult(KIO::Job *job)
 {
-  if (event)
-  {
-    kdDebug(5900) << "ADCalendar::setEventHandled(" << event->uid() << ")\n";
-    EventsMap::Iterator it = eventsHandled_.find(event->uid());
-    if (it != eventsHandled_.end())
-    {
-      // Update the existing entry for the event
-      it.data().alarmTimes = alarmtimes;
-      it.data().eventSequence = event->revision();
-    }
-    else
-      eventsHandled_.insert(event->uid(),EventItem(urlString(),
-                                                   event->revision(),
-                                                   alarmtimes));
-  }
+	if (job->error())
+	{
+		KURL url(mUrlString);
+		kdDebug(5900) << "Error downloading calendar from " << url.prettyURL() << endl;
+		job->showErrorDialog(0);
+	}
+	else
+	{
+		kdDebug(5900) << "--- Downloaded to " << mTempFileName << endl;
+		loadLocalFile(mTempFileName);
+	}
+	unlink(QFile::encodeName(mTempFileName));
+	mTempFileName = QString::null;
+	emit loaded(this, mLoaded);
 }
 
-/*
- * Clear all memory of events handled for the specified calendar.
- */
-void ADCalendar::clearEventsHandled(const QString& calendarURL)
+void ADCalendar::loadLocalFile(const QString& filename)
 {
-  for (EventsMap::Iterator it = eventsHandled_.begin();  it != eventsHandled_.end();  )
-  {
-    if (it.data().calendarURL == calendarURL)
-    {
-      EventsMap::Iterator i = it;
-      ++it;                      // prevent iterator becoming invalid with remove()
-      eventsHandled_.remove(i);
-    }
-    else
-      ++it;
-  }
+	mLoaded = load(filename);
+	if (!mLoaded)
+		kdDebug(5900) << "ADCalendar::loadLocalFile(): Error loading calendar file '" << filename << "'\n";
+	else
+		clearEventsHandled(true);   // remove all events which no longer exist from handled list
+}
+
+bool ADCalendar::setLoadedConnected()
+{
+	if (mLoadedConnected)
+		return true;
+	mLoadedConnected = true;
+	return false;
+}
+
+/******************************************************************************
+* Check whether all the alarms for the event with the given ID have already
+* been handled.
+*/
+bool ADCalendar::eventHandled(const KCal::Event* event, const QValueList<QDateTime>& alarmtimes)
+{
+	EventsMap::ConstIterator it = mEventsHandled.find(event->uid());
+	if (it == mEventsHandled.end())
+		return false;
+
+	int oldCount = it.data().alarmTimes.count();
+	int count = alarmtimes.count();
+	for (int i = 0;  i < count;  ++i)
+	{
+		if (alarmtimes[i].isValid()
+		&&  (i >= oldCount                             // is it an additional alarm?
+		     || !it.data().alarmTimes[i].isValid()     // or has it just become due?
+		     || it.data().alarmTimes[i].isValid()      // or has it changed?
+		        && alarmtimes[i] != it.data().alarmTimes[i]))
+			return false;     // this alarm has changed
+	}
+	return true;
+}
+
+/******************************************************************************
+* Remember that the specified alarms for the event with the given ID have been
+* handled.
+*/
+void ADCalendar::setEventHandled(const KCal::Event* event, const QValueList<QDateTime>& alarmtimes)
+{
+	if (event)
+	{
+		kdDebug(5900) << "ADCalendar::setEventHandled(" << event->uid() << ")\n";
+		EventsMap::Iterator it = mEventsHandled.find(event->uid());
+		if (it != mEventsHandled.end())
+		{
+			// Update the existing entry for the event
+			it.data().alarmTimes = alarmtimes;
+			it.data().eventSequence = event->revision();
+		}
+		else
+			mEventsHandled.insert(event->uid(),
+			                      EventItem(mUrlString, event->revision(), alarmtimes));
+	}
+}
+
+/******************************************************************************
+* Clear all memory of events handled for the calendar.
+*/
+void ADCalendar::clearEventsHandled(bool nonexistentOnly)
+{
+	for (EventsMap::Iterator it = mEventsHandled.begin();  it != mEventsHandled.end();  )
+	{
+		if (it.data().calendarURL == mUrlString
+		&&  (!nonexistentOnly  ||  !event(it.key())))
+		{
+			EventsMap::Iterator i = it;
+			++it;                      // prevent iterator becoming invalid with remove()
+			mEventsHandled.remove(i);
+		}
+		else
+			++it;
+	}
+}
+
+/******************************************************************************
+* Look up the calendar with the specified full calendar URL.
+*/
+ADCalendar* ADCalendar::getCalendar(const QString& calendarURL)
+{
+	if (!calendarURL.isEmpty())
+	{
+		for (ConstIterator it = begin();  it != end();  ++it)
+		{
+			if ((*it)->urlString() == calendarURL)
+				return *it;
+		}
+	}
+	return 0;
 }
--- kalarmd/adcalendar.h
+++ kalarmd/adcalendar.h
@@ -1,62 +1,105 @@
 /*
-    Calendar access for KAlarm Alarm Daemon.
+ *  adcalendar.h  -  calendar file access
+ *  Program:  KAlarm's alarm daemon (kalarmd)
+ *  (C) 2001, 2004 by David Jarvie <software@astrojar.org.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
 
-    This file is part of the KAlarm alarm daemon.
-    Copyright (c) 2001 David Jarvie <software@astrojar.org.uk>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-    As a special exception, permission is given to link this program
-    with any edition of Qt, and distribute the resulting executable,
-    without including the source code for Qt in the source distribution.
-*/
 #ifndef ADCALENDAR_H
 #define ADCALENDAR_H
 
-#include "adcalendarbase.h"
+#include <libkcal/calendarlocal.h>
+namespace KIO { class Job; }
+class ADCalendar;
+
 
 // Alarm Daemon calendar access
-class ADCalendar : public ADCalendarBase
+class ADCalendar : public KCal::CalendarLocal
 {
-  public:
-    ADCalendar(const QString& url, const QCString& appname);
-    ~ADCalendar()  { }
-    ADCalendar *create(const QString& url, const QString& appname);
+		Q_OBJECT
+	public:
+		typedef QValueList<ADCalendar*>::ConstIterator ConstIterator;
 
-    void           setEnabled( bool enabled ) { enabled_ = enabled; }
-    bool           enabled() const     { return enabled_ && !unregistered(); }
+		~ADCalendar();
 
-    void           setAvailable( bool ) {}
-    bool           available() const   { return loaded() && !unregistered(); }
+		const QString&  urlString() const       { return mUrlString; }
+		const QCString& appName() const         { return mAppName; }
 
-    bool           eventHandled(const Event*, const QValueList<QDateTime> &);
-    void           setEventHandled(const Event*,
-                                   const QValueList<QDateTime> &);
-    static void    clearEventsHandled(const QString& calendarURL);
+		void            setEnabled(bool enabled) { mEnabled = enabled; }
+		bool            enabled() const         { return mEnabled && !unregistered(); }
+		bool            available() const       { return loaded() && !unregistered(); }
 
-    bool           loadFile()          { return loadFile_(); }
+		// Client has registered since calendar was constructed, but
+		// has not since added the calendar. Monitoring is disabled.
+		void            setUnregistered(bool u) { mUnregistered = u; }
+		bool            unregistered() const    { return mUnregistered; }
+  
+		bool            eventHandled(const KCal::Event*, const QValueList<QDateTime>&);
+		void            setEventHandled(const KCal::Event*, const QValueList<QDateTime>&);
+		void            clearEventsHandled(bool nonexistentOnly = false);
 
-  public:
-    bool           available_;
-    bool           enabled_;       // events are currently manually enabled
+		bool            loadFile(bool reset);
+		bool            setLoadedConnected();     // check status of mLoadedConnected and set it to true
+		bool            downloading() const     { return !mTempFileName.isNull(); }
+		bool            loaded() const          { return mLoaded; }
+
+
+		static ConstIterator begin()            { return mCalendars.begin(); }
+		static ConstIterator end()              { return mCalendars.end(); }
+		static ADCalendar*   getCalendar(const QString& calendarURL);
+
+	signals:
+		void            loaded(ADCalendar*, bool success);
+
+	protected:
+		// Only ClientInfo can construct ADCalendar objects
+		friend class ClientInfo;
+		ADCalendar(const QString& url, const QCString& appname);
+
+	private slots:
+		void            slotDownloadJobResult(KIO::Job*);
+
+	private:
+		struct EventItem
+		{
+			EventItem() : eventSequence(0) { }
+			EventItem(const QString& url, int seqno, const QValueList<QDateTime>& alarmtimes)
+			        : calendarURL(url), eventSequence(seqno), alarmTimes(alarmtimes) {}
+
+			QString   calendarURL;
+			int       eventSequence;
+			QValueList<QDateTime> alarmTimes;
+		};
+
+		typedef QMap<QString, EventItem>  EventsMap;   // event ID, calendar URL/event sequence num
+		static EventsMap  mEventsHandled; // IDs of displayed KALARM type events
+		static QValueList<ADCalendar*> mCalendars;    // list of all constructed calendars
+
+		ADCalendar(const ADCalendar&);             // prohibit copying
+		ADCalendar& operator=(const ADCalendar&);  // prohibit copying
+
+		void            loadLocalFile(const QString& filename);
+
+		QString           mUrlString;     // calendar file URL
+		QCString          mAppName;       // name of application owning this calendar
+		QString           mTempFileName;  // temporary file used if currently downloading, else null
+		bool              mLoaded;        // true if calendar file is currently loaded
+		bool              mLoadedConnected; // true if the loaded() signal has been connected to AlarmDaemon
+		bool              mUnregistered;  // client has registered, but has not since added the calendar
+		bool              mEnabled;       // events are currently manually enabled
 };
 
-class ADCalendarFactory : public ADCalendarBaseFactory
-{
-  public:
-    ADCalendar *create(const QString& url, const QCString& appname);
-};
-
-#endif
+#endif // ADCALENDAR_H
