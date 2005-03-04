f5e34f2621481f3f33b63ff668bfbf11a4a78668
David Jarvie <djarvie@kde.org>
2005-03-04 20:26:19 +0000

    Add doxygen comments
    svn path=/trunk/kdepim/; revision=394927

--- widgets/lineedit.h
+++ widgets/lineedit.h
@@ -24,24 +24,59 @@
 #include <klineedit.h>
 
 
-/*=============================================================================
-= Class LineEdit
-= Line edit which accepts drag and drop of text, URLs and/or email addresses.
-* It has an option to prevent its contents being selected when it receives
-= focus.
-=============================================================================*/
-
+/**
+ *  The LineEdit class is a line edit widget which accepts specified types of drag and
+ *  drop content.
+ *
+ *  The widget will always accept drag and drop of text, except text/calendar mime type,
+ *  and of URLs. It will accept additional mime types depending on its configuration:
+ *  Text type accepts email address lists.
+ *  Email type accepts email address lists and VCard data (e.g. from KAddressBook). 
+ *
+ *  The class also provides an option to prevent its contents being selected when the
+ *  widget receives focus.
+ *
+ *  @short Line edit widget with extra drag and drop options.
+ *  @author David Jarvie <software@astrojar.org.uk>
+ */
 class LineEdit : public KLineEdit
 {
 		Q_OBJECT
 	public:
+		/** Types of drag and drop content which will be accepted.
+		 *  @li Text   - the line edit contains general text. It accepts text, a URL
+		 *               or an email from KMail (the subject line is used). If multiple
+		 *               URLs or emails are dropped, only the first is used; the
+		 *               rest are ignored.
+		 *  @li Url    - the line edit contains a URL. It accepts text or a URL. If
+		 *               multiple URLs are dropped, only the first URL is used; the
+		 *               rest are ignored.
+		 *  @li Emails - the line edit contains email addresses. It accepts text,
+		 *               mailto: URLs, emails from KMail (the From address is used)
+		 *               or vcard data (e.g. from KAddressBook). If multiple emails
+		 *               are dropped, only the first is used; the rest are ignored.
+		 *               
+		 */
 		enum Type { Text, Url, Emails };
-		explicit LineEdit(Type, QWidget* parent = 0, const char* name = 0);
+		/** Constructor.
+		 *  @param type The content type for the line edit.
+		 *  @param parent The parent object of this widget.
+		 *  @param name The name of this widget.
+		 */
+		explicit LineEdit(Type type, QWidget* parent = 0, const char* name = 0);
+		/** Constructs a line edit whose content type is Text.
+		 *  @param parent The parent object of this widget.
+		 *  @param name The name of this widget.
+		 */
 		explicit LineEdit(QWidget* parent = 0, const char* name = 0);
+		/** Prevents the line edit's contents being selected when the widget receives focus. */
 		void         setNoSelect()   { mNoSelect = true; }
-		void         setCursorAtEnd(bool y = true)  { mSetCursorAtEnd = y; }
+		/** Sets whether the cursor should be set at the beginning or end of the text when
+		 *  @see setText() is called. */
+		void         setCursorAtEnd(bool end = true)  { mSetCursorAtEnd = end; }
 	public slots:
-		virtual void setText(const QString&);
+		/** Sets the contents of the line edit to be @p str. */
+		virtual void setText(const QString& str);
 	protected:
 		virtual void focusInEvent(QFocusEvent*);
 		virtual void dragEnterEvent(QDragEnterEvent*);
