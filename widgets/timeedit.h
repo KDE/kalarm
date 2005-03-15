f9bacb3de9f0a1679743f29754fb9a5e62385014
David Jarvie <djarvie@kde.org>
2005-03-15 13:59:35 +0000

    Improve documentation
    svn path=/trunk/kdepim/; revision=397809

--- widgets/timeedit.h
+++ widgets/timeedit.h
@@ -57,13 +57,13 @@ class TimeEdit : public QHBox
 		explicit TimeEdit(QWidget* parent = 0, const char* name = 0);
 		/** Returns true if the widget is read only. */
 		bool          isReadOnly() const           { return mReadOnly; }
-		/** Sets whether the spin box is read-only for the user. If read-only,
+		/** Sets whether the widget is read-only for the user. If read-only,
 		 *  the time cannot be edited and the spin buttons and am/pm combo box
 		 *  are inactive.
 		 *  @param readOnly True to set the widget read-only, false to set it read-write.
 		 */
 		void          setReadOnly(bool readOnly);
-		/** Returns true if the spin box contains a valid value. */
+		/** Returns true if the widget contains a valid value. */
 		bool          isValid() const;
 		/** Sets whether the edit value is valid.
 		 *  If newly invalid, the value is displayed as asterisks.
@@ -81,25 +81,25 @@ class TimeEdit : public QHBox
 		 *  @param on True to enable wrapping, else false.
 		 */
 		void          setWrapping(bool on);
-		/** Returns the minimum value of the spin box in minutes. */
+		/** Returns the minimum value of the widget in minutes. */
 		int           minValue() const;
-		/** Returns the maximum value of the spin box in minutes. */
+		/** Returns the maximum value of the widget in minutes. */
 		int           maxValue() const;
-		/** Returns the maximum value of the spin box as a QTime value. */
+		/** Returns the maximum value of the widget as a QTime value. */
 		QTime         maxTime() const              { int mv = maxValue();  return QTime(mv/60, mv%60); }
-		/** Sets the minimum value of the spin box. */
+		/** Sets the minimum value of the widget. */
 		void          setMinValue(int minutes);
-		/** Sets the maximum value of the spin box. */
+		/** Sets the maximum value of the widget. */
 		void          setMaxValue(int minutes);
-		/** Sets the maximum value of the spin box. */
+		/** Sets the maximum value of the widget. */
 		void          setMaxValue(const QTime& time)  { setMaxValue(time.hour()*60 + time.minute()); }
 	public slots:
-		/** Sets the value of the spin box. */
+		/** Sets the value of the widget. */
 		virtual void  setValue(int minutes);
-		/** Sets the value of the spin box. */
+		/** Sets the value of the widget. */
 		void          setValue(const QTime& t)     { setValue(t.hour()*60 + t.minute()); }
 	signals:
-		/** This signal is emitted every time the value of the spin box changes
+		/** This signal is emitted every time the value of the widget changes
 		 *  (for whatever reason).
 		 *  @param minutes The new value.
 		 */
@@ -111,11 +111,11 @@ class TimeEdit : public QHBox
 	private:
 		void          setAmPmCombo(int am, int pm);
 
-		TimeSpinBox*  mSpinBox;
+		TimeSpinBox*  mSpinBox;       // always holds the 24-hour time
 		ComboBox*     mAmPm;
-		int           mAmIndex;            // mAmPm index to "am", or -1 if none
-		int           mPmIndex;            // mAmPm index to "pm", or -1 if none
-		bool          mReadOnly;           // the widget is read only
+		int           mAmIndex;       // mAmPm index to "am", or -1 if none
+		int           mPmIndex;       // mAmPm index to "pm", or -1 if none
+		bool          mReadOnly;      // the widget is read only
 };
 
 #endif // TIMEEDIT_H
