fa746f3faba86b5c5d81c1ac9cb44a5327e9d532
David Jarvie <djarvie@kde.org>
2005-04-07 18:05:40 +0000

    Add command script and execute-in-terminal enum options to DCOP interface
    svn path=/branches/KDE_3_4_BRANCH/kdepim/; revision=403844

--- dcophandler.cpp
+++ dcophandler.cpp
@@ -1,7 +1,7 @@
 /*
  *  dcophandler.cpp  -  handler for DCOP calls by other applications
  *  Program:  kalarm
- *  (C) 2002 - 2004 by David Jarvie <software@astrojar.org.uk>
+ *  (C) 2002 - 2005 by David Jarvie <software@astrojar.org.uk>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -352,6 +352,8 @@ unsigned DcopHandler::convertStartFlags(const DateTime& start, unsigned flags)
 	if (flags & REPEAT_SOUND)    kaEventFlags |= KAEvent::REPEAT_SOUND;
 	if (flags & AUTO_CLOSE)      kaEventFlags |= KAEvent::AUTO_CLOSE;
 	if (flags & EMAIL_BCC)       kaEventFlags |= KAEvent::EMAIL_BCC;
+	if (flags & SCRIPT)          kaEventFlags |= KAEvent::SCRIPT;
+	if (flags & EXEC_IN_XTERM)   kaEventFlags |= KAEvent::EXEC_IN_XTERM;
 	if (flags & DISABLED)        kaEventFlags |= KAEvent::DISABLED;
 	if (start.isDateOnly())      kaEventFlags |= KAEvent::ANY_TIME;
 	return kaEventFlags;
