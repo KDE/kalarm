16a430caf5197002b599b98feeb66d4776d7fdee
David Jarvie <djarvie@kde.org>
2005-05-20 06:27:57 +0000

    Add facility to write to stdin
    svn path=/trunk/KDE/kdepim/kalarm/widgets/; revision=415981

---
+++ lib/shellprocess.cpp
@@ -0,0 +1,208 @@
+/*
+ *  shellprocess.cpp  -  execute a shell process
+ *  Program:  kalarm
+ *  (C) 2004, 2005 by David Jarvie <software@astrojar.org.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <kapplication.h>
+#include <klocale.h>
+#include <kdebug.h>
+
+#include "shellprocess.moc"
+
+
+QCString  ShellProcess::mShellName;
+bool      ShellProcess::mInitialised = false;
+bool      ShellProcess::mAuthorised  = false;
+
+
+ShellProcess::ShellProcess(const QString& command)
+	: KShellProcess(shellName()),
+	  mCommand(command),
+	  mStatus(INACTIVE),
+	  mStdinExit(false)
+{
+}
+
+/******************************************************************************
+* Execute a command.
+*/
+bool ShellProcess::start(Communication comm)
+{
+	if (!authorised())
+	{
+		mStatus = UNAUTHORISED;
+		return false;
+	}
+	KShellProcess::operator<<(mCommand);
+	connect(this, SIGNAL(wroteStdin(KProcess*)), SLOT(writtenStdin(KProcess*)));
+	connect(this, SIGNAL(processExited(KProcess*)), SLOT(slotExited(KProcess*)));
+	if (!KShellProcess::start(KProcess::NotifyOnExit, comm))
+	{
+		mStatus = START_FAIL;
+		return false;
+	}
+	mStatus = RUNNING;
+	return true;
+}
+
+/******************************************************************************
+* Called when a shell process execution completes.
+* Interprets the exit status according to which shell was called, and emits
+* a shellExited() signal.
+*/
+void ShellProcess::slotExited(KProcess* proc)
+{
+	kdDebug(5950) << "ShellProcess::slotExited()\n";
+	mStdinQueue.clear();
+	mStatus = SUCCESS;
+	if (!proc->normalExit())
+	{
+		kdWarning(5950) << "ShellProcess::slotExited(" << mCommand << ") " << mShellName << ": died/killed\n";
+		mStatus = DIED;
+	}
+	else
+	{
+		// Some shells report if the command couldn't be found, or is not executable
+		int status = proc->exitStatus();
+		if (mShellName == "bash"  &&  (status == 126 || status == 127)
+		||  mShellName == "ksh"  &&  status == 127)
+		{
+			kdWarning(5950) << "ShellProcess::slotExited(" << mCommand << ") " << mShellName << ": not found or not executable\n";
+			mStatus = NOT_FOUND;
+		}
+	}
+	emit shellExited(this);
+}
+
+/******************************************************************************
+* Write a string to STDIN.
+*/
+void ShellProcess::writeStdin(const char* buffer, int bufflen)
+{
+	QCString scopy(buffer, bufflen+1);    // construct a deep copy
+	bool write = !mStdinQueue.count();
+	mStdinQueue.append(scopy);
+	if (write)
+		KProcess::writeStdin(mStdinQueue.first(), mStdinQueue.first().length());
+}
+
+/******************************************************************************
+* Called when output to STDIN completes.
+* Send the next queued output, if any.
+* Note that buffers written to STDIN must not be freed until the writtenStdin()
+* signal has been processed.
+*/
+void ShellProcess::writtenStdin(KProcess* proc)
+{
+	mStdinQueue.pop_front();   // free the buffer which has now been written
+	if (mStdinQueue.count())
+		proc->writeStdin(mStdinQueue.first(), mStdinQueue.first().length());
+	else if (mStdinExit)
+		kill();
+}
+
+/******************************************************************************
+* Tell the process to exit once all STDIN strings have been written.
+*/
+void ShellProcess::stdinExit()
+{
+	if (mStdinQueue.isEmpty())
+		kill();
+	else
+		mStdinExit = true;
+}
+
+/******************************************************************************
+* Return the error message corresponding to the command exit status.
+* Reply = null string if not yet exited, or if command successful.
+*/
+QString ShellProcess::errorMessage() const
+{
+	switch (mStatus)
+	{
+		case UNAUTHORISED:
+			return i18n("Failed to execute command (shell access not authorized):");
+		case START_FAIL:
+		case NOT_FOUND:
+			return i18n("Failed to execute command:");
+		case DIED:
+			return i18n("Command execution error:");
+		case INACTIVE:
+		case RUNNING:
+		case SUCCESS:
+		default:
+			return QString::null;
+	}
+}
+
+/******************************************************************************
+* Find which shell to use.
+* This is a duplication of what KShellProcess does, but we need to know
+* which shell is used in order to decide what its exit code means.
+*/
+const QCString& ShellProcess::shellName()
+{
+	if (mShellName.isEmpty())
+	{
+		QCString shell = "/bin/sh";
+		QCString envshell = QCString(getenv("SHELL")).stripWhiteSpace();
+		if (!envshell.isEmpty())
+		{
+			struct stat fileinfo;
+			if (stat(envshell.data(), &fileinfo) != -1  // ensure file exists
+			&&  !S_ISDIR(fileinfo.st_mode)              // and it's not a directory
+			&&  !S_ISCHR(fileinfo.st_mode)              // and it's not a character device
+			&&  !S_ISBLK(fileinfo.st_mode)              // and it's not a block device
+#ifdef S_ISSOCK
+			&&  !S_ISSOCK(fileinfo.st_mode)             // and it's not a socket
+#endif
+			&&  !S_ISFIFO(fileinfo.st_mode)             // and it's not a fifo
+			&&  !access(envshell.data(), X_OK))         // and it's executable
+				shell = envshell;
+		}
+		// Get the shell filename with the path stripped
+		mShellName = shell;
+		int i = mShellName.findRev('/');
+		if (i >= 0)
+			mShellName = mShellName.mid(i + 1);
+	}
+	return mShellName;
+}
+
+/******************************************************************************
+* Check whether shell commands are allowed at all.
+*/
+bool ShellProcess::authorised()
+{
+	if (!mInitialised)
+	{
+#if KDE_VERSION >= 290
+		mAuthorised = kapp->authorize("shell_access");
+#else
+		mAuthorised = true;
+#endif
+		mInitialised = true;
+	}
+	return mAuthorised;
+}
---
+++ lib/shellprocess.h
@@ -0,0 +1,124 @@
+/*
+ *  shellprocess.h  -  execute a shell process
+ *  Program:  kalarm
+ *  (C) 2004, 2005 by David Jarvie <software@astrojar.org.uk>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef SHELLPROCESS_H
+#define SHELLPROCESS_H
+
+#include <kprocess.h>
+
+
+/**
+ *  The ShellProcess class runs a shell command and interprets the shell exit status
+ *  as far as possible. It blocks execution if shell access is prohibited.
+ *
+ *  Before executing any command, ShellProcess checks whether shell commands are
+ *  allowed at all. If not (e.g. if the user is running in kiosk mode), it blocks
+ *  execution.
+ *
+ *  Derived from KShellProcess, this class additionally tries to interpret the shell
+ *  exit status. Different shells use different exit codes. Currently, if bash or ksh
+ *  report that the command could not be found or could not be executed, the NOT_FOUND
+ *  status is returned.
+ *
+ *  @short KShellProcess with interpretation of shell exit status.
+ *  @author David Jarvie <software@astrojar.org.uk>
+ */
+class ShellProcess : public KShellProcess
+{
+		Q_OBJECT
+	public:
+		/** Current status of the shell process.
+		 *  @li INACTIVE - start() has not yet been called to run the command.
+		 *  @li RUNNING - the command is currently running.
+		 *  @li SUCCESS - the command appears to have exited successfully.
+		 *  @li UNAUTHORISED - shell commands are not authorised for this user.
+		 *  @li DIED - the command didn't exit cleanly, i.e. was killed or died.
+		 *  @li NOT_FOUND - the command was either not found or not executable.
+		 *  @li START_FAIL - the command couldn't be started for other reasons.
+		 */
+		enum Status {
+			INACTIVE,     // start() has not yet been called to run the command
+			RUNNING,      // command is currently running
+			SUCCESS,      // command appears to have exited successfully
+			UNAUTHORISED, // shell commands are not authorised for this user
+			DIED,         // command didn't exit cleanly, i.e. was killed or died
+			NOT_FOUND,    // command either not found or not executable
+			START_FAIL    // command couldn't be started for other reasons
+		};
+		/** Constructor.
+		 *  @param command The command line to be run when start() is called.
+		 */
+		ShellProcess(const QString& command);
+		/** Executes the configured command.
+		 *  @param comm Which communication links should be established to the child process
+		 *  (stdin/stdout/stderr).
+		 */
+		bool            start(Communication comm = NoCommunication);
+		/** Returns the current status of the shell process. */
+		Status          status() const       { return mStatus; }
+		/** Returns whether the command was run successfully.
+		 *  @return True if the command has been run and appears to have exited successfully.
+		 */
+		bool            normalExit() const   { return mStatus == SUCCESS; }
+		/** Returns the command configured to be run. */
+		const QString&  command() const      { return mCommand; }
+		/** Returns the error message corresponding to the command exit status.
+		 *  @return Error message if an error occurred. Null string if the command has not yet
+		 *          exited, or if the command ran successfully.
+		 */
+		QString         errorMessage() const;
+		/** Writes a string to the process's STDIN. */
+		void            writeStdin(const char* buffer, int bufflen);
+		/** Tell the process to exit once any outstanding STDIN strings have been written. */
+		void            stdinExit();
+		/** Returns whether the user is authorised to run shell commands. Shell commands may
+		 *  be prohibited in kiosk mode, for example.
+		 */
+		static bool     authorised();
+		/** Finds which shell to use. */
+		static const QCString& shellName();
+
+	signals:
+		/** Signal emitted when the shell process execution completes. It is not emitted
+		 *  if start() did not attempt to start the command execution, e.g. in kiosk mode.
+		 */
+		void  shellExited(ShellProcess*);
+
+	private slots:
+		void  writtenStdin(KProcess*);
+		void  slotExited(KProcess*);
+
+	private:
+		// Prohibit the following inherited methods
+		ShellProcess&  operator<<(const QString&);
+		ShellProcess&  operator<<(const QCString&);
+		ShellProcess&  operator<<(const QStringList&);
+		ShellProcess&  operator<<(const char*);
+
+		static QCString      mShellName;    // name of shell to be used
+		static bool          mInitialised;  // true once static data has been initialised
+		static bool          mAuthorised;   // true if shell commands are authorised
+		QString              mCommand;      // copy of command to be executed
+		QValueList<QCString> mStdinQueue;   // queued strings to send to STDIN
+		Status               mStatus;       // current execution status
+		bool                 mStdinExit;    // exit once STDIN queue has been written
+};
+
+#endif // SHELLPROCESS_H
