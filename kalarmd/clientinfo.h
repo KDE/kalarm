9bda5d64fe7cde3cd5548a93795cb1ea8ff83e2a
David Jarvie <djarvie@kde.org>
2005-08-27 18:30:33 +0000

    Update copyright statement
    svn path=/branches/KDE/3.5/kdepim/; revision=454013

--- kalarmd/clientinfo.h
+++ kalarmd/clientinfo.h
@@ -1,7 +1,7 @@
 /*
  *  clientinfo.h  -  client application information
  *  Program:  KAlarm's alarm daemon (kalarmd)
- *  (C) 2001, 2004 by David Jarvie <software@astrojar.org.uk>
+ *  Copyright (C) 2001, 2004 by David Jarvie <software@astrojar.org.uk>
  *  Based on the original, (c) 1998, 1999 Preston Brown
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -14,9 +14,9 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
 #ifndef _CALCLIENT_H
