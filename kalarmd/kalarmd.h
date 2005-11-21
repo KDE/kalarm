253cbb22d6698feb351b7694b0bd07181db143bd
David Jarvie <djarvie@kde.org>
2005-11-21 18:49:04 +0000

    Revert unintentional change
    svn path=/trunk/KDE/kdepim/; revision=482099

--- kalarmd/kalarmd.h
+++ kalarmd/kalarmd.h
@@ -25,8 +25,6 @@
 #include <config.h>
 #endif
 
-#undef QT3_SUPPORT
-
 #define DAEMON_VERSION            "4.1"         // kalarmd version number
 #define DAEMON_APP_NAME           "kalarmd"     // DCOP name of alarm daemon application
 #define DAEMON_DCOP_OBJECT        "ad"          // DCOP name of kalarmd's DCOP interface
