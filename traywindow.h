8bc2a61c9ada25602eebb539550befbc37422a57
David Jarvie <djarvie@kde.org>
2006-02-11 15:39:32 +0000

    Remove obsolete comment
    svn path=/trunk/KDE/kdepim/; revision=508388

--- traywindow.h
+++ traywindow.h
@@ -1,7 +1,7 @@
 /*
  *  traywindow.h  -  the KDE system tray applet
  *  Program:  kalarm
- *  Copyright (c) 2002 - 2004 by David Jarvie <software@astrojar.org.uk>
+ *  Copyright (c) 2002-2004 by David Jarvie <software@astrojar.org.uk>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -21,7 +21,6 @@
 #ifndef TRAYWINDOW_H
 #define TRAYWINDOW_H
 
-//Added by qt3to4:
 #include <QPixmap>
 #include <ksystemtray.h>
 
