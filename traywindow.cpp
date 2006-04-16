fcbac03ffee29c6f9c22be4f1ef6461c9f457d9f
David Jarvie <djarvie@kde.org>
2006-04-16 18:44:41 +0000

    Bug 125675: Don't show disabled alarms in system tray tooltip alarm list
    svn path=/branches/KDE/3.5/kdepim/; revision=530457

--- traywindow.cpp
+++ traywindow.cpp
@@ -238,7 +238,7 @@ void TrayWindow::tooltipAlarmText(QString& text) const
 	{
 		KCal::Event* kcalEvent = *it;
 		event.set(*kcalEvent);
-		if (!event.expired()  &&  event.action() == KAEvent::MESSAGE)
+		if (event.enabled()  &&  !event.expired()  &&  event.action() == KAEvent::MESSAGE)
 		{
 			TipItem item;
 			DateTime dateTime = event.nextDateTime(false);
