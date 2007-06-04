84b928d934511a65675ab0d04c37da827689249f
David Jarvie <djarvie@kde.org>
2007-06-04 20:07:06 +0000

    Format
    svn path=/trunk/KDE/kdepim/; revision=671448

--- resources/alarmresources.h
+++ resources/alarmresources.h
@@ -294,10 +294,10 @@ class KDE_EXPORT AlarmResources : public KCal::Calendar, public KRES::ManagerObs
 		*/
 		virtual bool deleteEvent(KCal::Event* event);
 
-                /**
-                  Removes all Events from the calendar.
-                */
-                virtual void deleteAllEvents() {}
+		/**
+		   Removes all Events from the calendar.
+		*/
+		virtual void deleteAllEvents() {}
 
 		/**
 		   Return a sorted, unfiltered list of all Events.
@@ -440,13 +440,13 @@ class KDE_EXPORT AlarmResources : public KCal::Calendar, public KRES::ManagerObs
 		// Override unused virtual functions
 		virtual bool addTodo(KCal::Todo*) { return true; }
 		virtual bool deleteTodo(KCal::Todo*) { return true; }
-                virtual void deleteAllTodos() {}
+		virtual void deleteAllTodos() {}
 		virtual KCal::Todo::List rawTodos(KCal::TodoSortField = KCal::TodoSortUnsorted, KCal::SortDirection = KCal::SortDirectionAscending) { return KCal::Todo::List(); }
 		virtual KCal::Todo::List rawTodosForDate(const QDate&) { return KCal::Todo::List(); }
 		virtual KCal::Todo* todo(const QString&) { return 0; }
 		virtual bool addJournal(KCal::Journal*) { return true; }
 		virtual bool deleteJournal(KCal::Journal*) { return true; }
-                virtual void deleteAllJournals() {}
+		virtual void deleteAllJournals() {}
 		virtual KCal::Journal::List rawJournals(KCal::JournalSortField = KCal::JournalSortUnsorted, KCal::SortDirection = KCal::SortDirectionAscending) { return KCal::Journal::List(); }
 		virtual KCal::Journal::List rawJournalsForDate(const QDate&) { return KCal::Journal::List(); }
 		virtual KCal::Journal* journal(const QString&) { return 0; }
