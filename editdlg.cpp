195eb392830c503edf43931871c708bb61426114
David Jarvie <djarvie@kde.org>
2007-11-10 13:12:04 +0000

    Fix next occurrence time set after editing alarm, when it's a sub-repetition
    svn path=/branches/KDE/3.5/kdepim/; revision=734909

--- editdlg.cpp
+++ editdlg.cpp
@@ -1257,6 +1257,15 @@ void EditAlarmDlg::setEvent(KAEvent& event, const QString& text, bool trial)
 		if (mRecurrenceEdit->repeatType() != RecurrenceEdit::NO_RECUR)
 		{
 			mRecurrenceEdit->updateEvent(event, !mTemplate);
+			QDateTime now = QDateTime::currentDateTime();
+			bool dateOnly = mAlarmDateTime.isDateOnly();
+			if (dateOnly  &&  mAlarmDateTime.date() < now.date()
+			||  !dateOnly  &&  mAlarmDateTime.rawDateTime() < now)
+			{
+				// A timed recurrence has an entered start date which has
+				// already expired, so we must adjust the next repetition.
+				event.setNextOccurrence(now);
+			}
 			mAlarmDateTime = event.startDateTime();
 			if (mDeferDateTime.isValid()  &&  mDeferDateTime < mAlarmDateTime)
 			{
