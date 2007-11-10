cd99c9a19727f930e2914c23f46f35c2c8fac032
Pino Toscano <pino@kde.org>
2007-11-10 16:26:33 +0000

    icon renaming: - exec -> system-run
    svn path=/trunk/KDE/kdepim/akregator/; revision=734984

--- newalarmaction.cpp
+++ newalarmaction.cpp
@@ -36,7 +36,7 @@ NewAlarmAction::NewAlarmAction(bool templates, const QString& label, QObject* pa
 	setShortcuts(KStandardShortcut::openNew());
 	QAction* act = menu()->addAction(KIcon(QLatin1String("view-close")), (templates ? i18nc("@item:inmenu", "&Display Alarm Template") : i18nc("@item:inmenu", "Display Alarm")));
 	mTypes[act] = EditAlarmDlg::DISPLAY;
-	mCommandAction = menu()->addAction(KIcon(QLatin1String("exec")), (templates ? i18nc("@item:inmenu", "&Command Alarm Template") : i18nc("@item:inmenu", "Command Alarm")));
+	mCommandAction = menu()->addAction(KIcon(QLatin1String("system-run")), (templates ? i18nc("@item:inmenu", "&Command Alarm Template") : i18nc("@item:inmenu", "Command Alarm")));
 	mTypes[mCommandAction] = EditAlarmDlg::COMMAND;
 	act = menu()->addAction(KIcon(QLatin1String("mail")), (templates ? i18nc("@item:inmenu", "&Email Alarm Template") : i18nc("@item:inmenu", "Email Alarm")));
 	mTypes[act] = EditAlarmDlg::EMAIL;
