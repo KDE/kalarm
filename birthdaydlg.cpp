e865149defa95057e487dfbdeffdc6e239e81a0a
Hamish Rodda <rodda@kde.org>
2007-12-03 15:30:33 +0000

    KActionCollection::associateWidget -> addAssociatedWidget
    svn path=/trunk/KDE/kdepim/knode/; revision=744427

--- birthdaydlg.cpp
+++ birthdaydlg.cpp
@@ -35,6 +35,7 @@
 #include <kactioncollection.h>
 #include <khbox.h>
 #include <kdebug.h>
+#include <kdeversion.h>
 
 #include "alarmcalendar.h"
 #include "birthdaymodel.h"
@@ -211,7 +212,13 @@ BirthdayDlg::BirthdayDlg(QWidget* parent)
 	KActionCollection* actions = new KActionCollection(this);
 	KStandardAction::selectAll(mListView, SLOT(selectAll()), actions);
 	KStandardAction::deselect(mListView, SLOT(clearSelection()), actions);
-	actions->associateWidget(mListView);
+	actions->addAssociatedWidget(mListView);
+    foreach (QAction* action, actions->actions())
+#if QT_VERSION < KDE_MAKE_VERSION(4,4,0)
+        action->setShortcutContext(Qt::WidgetShortcut); // remove after Qt4.4 becomes mandatory
+#else
+        action->setShortcutContext(Qt::WidgetWithChildrenShortcut);
+#endif
 
 	enableButtonOk(false);     // only enable OK button when something is selected
 }
