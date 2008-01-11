3a2a04a861b44c345fafe0145fd00fa2702be67b
David Jarvie <djarvie@kde.org>
2008-01-11 18:51:24 +0000

    Krazy Qt class check
    svn path=/trunk/KDE/kdepim/; revision=760017

--- messagewin.cpp
+++ messagewin.cpp
@@ -1,7 +1,7 @@
 /*
  *  messagewin.cpp  -  displays an alarm message
  *  Program:  kalarm
- *  Copyright © 2001-2007 by David Jarvie <djarvie@kde.org>
+ *  Copyright © 2001-2008 by David Jarvie <djarvie@kde.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -432,7 +432,10 @@ void MessageWin::initView()
 						if (imageType)
 							view->setHtml("<img source=\"" + tmpFile + "\">");
 						else
-							view->QTextBrowser::setSource(tmpFile);   // if not an image, assume a text file
+						{
+							// If not an image, assume a text file
+							view->QTextBrowser::setSource(tmpFile);   //krazy:exclude=qclasses
+						}
 						view->setMinimumSize(view->sizeHint());
 						topLayout->addWidget(view);
 
