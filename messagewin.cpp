5c223ed97115ca2f40fffab0a977a4c93b6f85d3
Thomas McGuire <mcguire@kde.org>
2009-01-20 16:56:20 +0000

    Merged revisions 906260 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.1/kdepim
    ........
      r906260 | djarvie | 2009-01-05 21:12:43 +0100 (Mon, 05 Jan 2009) | 3 lines
      
      Fix invalid alarm remaining in calendar when pre-alarm action failure message
      is acknowledged before the alarm is deferred.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepim/; revision=914212

--- messagewin.cpp
+++ messagewin.cpp
@@ -1,7 +1,7 @@
 /*
  *  messagewin.cpp  -  displays an alarm message
  *  Program:  kalarm
- *  Copyright © 2001-2008 by David Jarvie <djarvie@kde.org>
+ *  Copyright © 2001-2009 by David Jarvie <djarvie@kde.org>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -1468,7 +1468,7 @@ void MessageWin::resizeEvent(QResizeEvent* re)
 void MessageWin::closeEvent(QCloseEvent* ce)
 {
 	// Don't prompt or delete the alarm from the display calendar if the session is closing
-	if (!theApp()->sessionClosingDown())
+	if (!mErrorWindow  &&  !theApp()->sessionClosingDown())
 	{
 		if (mConfirmAck  &&  !mNoCloseConfirm)
 		{
