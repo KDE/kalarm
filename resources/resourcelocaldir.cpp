09e0301a93e0b185b771285f1ff4eea2479cb6cc
David Jarvie <djarvie@kde.org>
2009-03-14 15:26:33 +0000

    After deleting, doSave was recreating the file again, so incidences never were deleted. (From SVN commit 938673 by smartins)
    svn path=/trunk/KDE/kdepim/; revision=939287

--- resources/resourcelocaldir.cpp
+++ resources/resourcelocaldir.cpp
@@ -1,8 +1,8 @@
 /*
  *  resourcelocaldir.cpp  -  KAlarm local directory calendar resource
  *  Program:  kalarm
- *  Copyright © 2006-2008 by David Jarvie <djarvie@kde.org>
- *  Based on resourcelocaldir.cpp in libkcal (updated to rev 779953),
+ *  Copyright © 2006-2009 by David Jarvie <djarvie@kde.org>
+ *  Based on resourcelocaldir.cpp in libkcal (updated to rev 779953, 938673),
  *  Copyright (c) 2003 Cornelius Schumacher <schumacher@kde.org>
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -367,6 +367,12 @@ bool KAResourceLocalDir::doSave(bool)
 
 bool KAResourceLocalDir::doSave(bool, Incidence* incidence)
 {
+	if (mDeletedIncidences.contains(incidence))
+	{
+		mDeletedIncidences.removeAll(incidence);
+		return true;
+	}
+
 	QString id = incidence->uid();
 	QString fileName = mURL.path() + '/' + id;
 	kDebug(KARES_DEBUG) << fileName;
@@ -404,6 +410,8 @@ bool KAResourceLocalDir::deleteEvent(Event* event)
 	clearChange(event);
 	disableChangeNotification();    // don't record this deletion as pending
 	bool success = calendar()->deleteEvent(event);
+	if (success)
+		mDeletedIncidences.append(event);
 	enableChangeNotification();
 	return success;
 }
