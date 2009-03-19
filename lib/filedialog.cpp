087189f4e88031ed8ceb289c92bc43a62136964f
David Jarvie <djarvie@kde.org>
2009-03-19 19:34:29 +0000

    Krazy fix
    svn path=/trunk/KDE/kdepim/kalarm/lib/; revision=941491

--- lib/filedialog.cpp
+++ lib/filedialog.cpp
@@ -18,7 +18,7 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include "kalarm.h"
+#include "kalarm.h"   //krazy:exclude=includes (kalarm.h must be first)
 
 #include "filedialog.h"
 #include <kabstractfilewidget.h>
