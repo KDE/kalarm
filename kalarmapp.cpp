9aae63db36dad1c600aa63c67198da3879a8bc52
David Jarvie <djarvie@kde.org>
2009-04-14 20:50:33 +0000

    Fix possible crash on exit
    svn path=/branches/KDE/4.2/kdepim/; revision=953964

--- kalarmapp.cpp
+++ kalarmapp.cpp
@@ -764,6 +764,7 @@ void KAlarmApp::quitIf(int exitCode, bool force)
 	{
 		// Quit regardless, except for message windows
 		mQuitting = true;
+		EventListModel::alarms()->clear();  // prevent view updates during window destruction
 		MainWindow::closeAll();
 		displayTrayIcon(false);
 		if (MessageWin::instanceCount())
