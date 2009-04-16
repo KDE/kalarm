372c4b95b9d552be9b46fd91747a97e81f4d380d
David Jarvie <djarvie@kde.org>
2009-04-16 17:43:45 +0000

    Add diagnostic output
    svn path=/trunk/KDE/kdepim/; revision=954992

--- kalarmapp.cpp
+++ kalarmapp.cpp
@@ -887,7 +887,9 @@ void KAlarmApp::checkNextDueAlarm()
 	if (!nextEvent)
 		return;   // there are no alarms pending
 	KDateTime nextDt = nextEvent->nextTrigger(KAEvent::ALL_TRIGGER).effectiveKDateTime();
-	qint64 interval = KDateTime::currentDateTime(Preferences::timeZone()).secsTo_long(nextDt);
+	KDateTime now = KDateTime::currentDateTime(Preferences::timeZone());
+	qint64 interval = now.secsTo_long(nextDt);
+	kDebug() << "now:" << qPrintable(now.toString("%Y-%m-%d %H:%M %:Z")) << ", next:" << qPrintable(nextDt.toString("%Y-%m-%d %H:%M %:Z")) << ", due:" << interval;
 	if (interval <= 0)
 	{
 		// Queue the alarm
