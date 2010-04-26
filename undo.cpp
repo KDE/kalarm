da6e849201e95fc34e64c7556a7f9af83549ce11
David Jarvie <djarvie@kde.org>
2010-04-26 21:53:26 +0000

    Krazy fix
    svn path=/trunk/KDE/kdepim/; revision=1119278

--- undo.cpp
+++ undo.cpp
@@ -18,7 +18,7 @@
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-#include "kalarm.h"
+#include "kalarm.h"   //krazy:exclude=includes (kalarm.h must be first)
 #include "undo.h"
 
 #include "alarmcalendar.h"
