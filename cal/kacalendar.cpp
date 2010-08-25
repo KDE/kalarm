e6c5a019466c92679c3b26e3e5d1548d32d20fb8
David Jarvie <djarvie@kde.org>
2010-08-25 00:11:24 +0000

    Install KAlarm mime types. Use the singular for mime type names.
    svn path=/trunk/KDE/kdepim/; revision=1167588

--- cal/kacalendar.cpp
+++ cal/kacalendar.cpp
@@ -53,10 +53,10 @@ namespace KAlarm
 {
 
 #ifdef USE_AKONADI
-const QLatin1String MIME_BASE("application/x-vnd.kde.alarms");
-const QLatin1String MIME_ACTIVE("application/x-vnd.kde.alarms.active");
-const QLatin1String MIME_ARCHIVED("application/x-vnd.kde.alarms.archived");
-const QLatin1String MIME_TEMPLATE("application/x-vnd.kde.alarms.template");
+const QLatin1String MIME_BASE("application/x-vnd.kde.alarm");
+const QLatin1String MIME_ACTIVE("application/x-vnd.kde.alarm.active");
+const QLatin1String MIME_ARCHIVED("application/x-vnd.kde.alarm.archived");
+const QLatin1String MIME_TEMPLATE("application/x-vnd.kde.alarm.template");
 #endif
 
 static const QByteArray VERSION_PROPERTY("VERSION");     // X-KDE-KALARM-VERSION VCALENDAR property
