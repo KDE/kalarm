c493a8e5ae7ff40135cd13102d0ac150caead903
David Jarvie <djarvie@kde.org>
2010-08-26 21:55:31 +0000

    Always update mEarliestAlarm when an alarm is deleted, to prevent possible crashes
    svn path=/branches/KDE/4.5/kdepim/; revision=1168533

--- alarmcalendar.cpp
+++ alarmcalendar.cpp
@@ -426,6 +426,7 @@ void AlarmCalendar::updateKAEvents(AlarmResource* resource, KCal::CalendarLocal*
 		delete event;
 	}
 	events.clear();
+	mEarliestAlarm[resource] = 0;
 	if (!cal)
 		return;
 
@@ -904,6 +905,8 @@ bool AlarmCalendar::addEvent(KAEvent* event, QWidget* promptParent, bool useEven
 			// Adding to mCalendar failed, so undo AlarmCalendar::addEvent()
 			mEventMap.remove(event->id());
 			mResourceMap[resource].removeAll(event);
+			if (mEarliestAlarm[resource] == event)
+				findEarliestAlarm(resource);
 		}
 		*event = oldEvent;
 		delete kcalEvent;
@@ -1086,9 +1089,8 @@ KAlarm::CalEvent::Type AlarmCalendar::deleteEventInternal(const QString& eventID
 		mEventMap.erase(it);
 		resource = AlarmResources::instance()->resource(kcalEvent);
 		mResourceMap[resource].removeAll(ev);
-		bool recalc = (mEarliestAlarm[resource] == ev);
 		delete ev;
-		if (recalc)
+		if (mEarliestAlarm[resource] == ev)
 			findEarliestAlarm(resource);
 	}
 	else
