168a7c8ca31acfe2121f27d285a76cc769042ce7
David Jarvie <djarvie@kde.org>
2011-08-02 00:42:15 +0100

    Load merged KAlarm Akonadi resource catalog
    (Should have been in commit 89ab6066107704b61b8e0cec05969a7f1045ba85.)

--- akonadi/common/kalarmresourcecommon.cpp
+++ akonadi/common/kalarmresourcecommon.cpp
@@ -70,7 +70,7 @@ void initialise(QObject* parent)
     AttributeFactory::registerAttribute<CompatibilityAttribute>();
     AttributeFactory::registerAttribute<EventAttribute>();
 
-    KGlobal::locale()->insertCatalog("akonadi_kalarm_resource_common");
+    KGlobal::locale()->insertCatalog("akonadi_kalarm_resource");
 }
 
 /******************************************************************************
