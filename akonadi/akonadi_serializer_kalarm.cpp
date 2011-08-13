af21aa5ebf42d41fae70c062ce19c57b648cb8e1
David Jarvie <djarvie@kde.org>
2011-08-13 12:32:44 +0100

    Add comments

--- akonadi/akonadi_serializer_kalarm.cpp
+++ akonadi/akonadi_serializer_kalarm.cpp
@@ -37,6 +37,7 @@
 using namespace Akonadi;
 
 
+// Convert from backend data stream to a KAEvent, and set it into the item's payload.
 bool SerializerPluginKAlarm::deserialize(Item& item, const QByteArray& label, QIODevice& data, int version)
 {
     Q_UNUSED(version);
@@ -86,6 +87,7 @@ bool SerializerPluginKAlarm::deserialize(Item& item, const QByteArray& label, QI
     return true;
 }
 
+// Convert an item's KAEvent payload to backend data stream.
 void SerializerPluginKAlarm::serialize(const Item& item, const QByteArray& label, QIODevice& data, int& version)
 {
     Q_UNUSED(version);
