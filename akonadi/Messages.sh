dffb64e048bcbf999bcf2e91bc8b747f84a40df5
David Jarvie <djarvie@kde.org>
2011-10-24 20:00:37 +0100

    Move kdepim/kalarm/akonadi directory to kdepim-runtime
    Moved to kdepim-runtime/resources/kalarm and kdepim-runtime/plugins.
    git commit ea2be886ca6a7ac211feea63e38927cc172ba92d.

--- akonadi/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.kcfg -o -name \*.ui` >> rc.cpp || exit 11
-$XGETTEXT `find . \( ! -path "./kdepim-runtime/*" \) -a \( -name "*.cpp" -o -name "*.h" \)` -o $podir/akonadi_kalarm_resource.pot
-rm -f rc.cpp
