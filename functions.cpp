f44270d3dbcd9818960559b4eac4d34b6bdd6cce
Dan Vrátil <dvratil@redhat.com>
2015-05-14 14:20:30 +0200

    Adapt to Dbus interface name change

--- functions.cpp
+++ functions.cpp
@@ -1947,7 +1947,7 @@ KAlarm::UpdateResult runKOrganizer()
     // If Kontact is running, there is a load() method which needs to be called to
     // load KOrganizer into Kontact. But if KOrganizer is running independently,
     // the load() method doesn't exist.
-    QDBusInterface iface(KORG_DBUS_SERVICE, QLatin1String(KORG_DBUS_LOAD_PATH), QLatin1String("org.kde.KUniqueApplication"));
+    QDBusInterface iface(KORG_DBUS_SERVICE, QLatin1String(KORG_DBUS_LOAD_PATH), QLatin1String("org.kde.PIMUniqueApplication"));
     if (!iface.isValid())
     {
         status.set(KAlarm::UPDATE_KORG_ERR, iface.lastError().message());
