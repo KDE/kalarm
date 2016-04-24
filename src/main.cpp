65f81284b8e9b6e36eed64531a28ec5f84e4c0c1
Montel Laurent <montel@kde.org>
2016-04-24 20:47:01 +0200

    We depend against qt5.6

--- src/main.cpp
+++ src/main.cpp
@@ -43,9 +43,7 @@ int main(int argc, char* argv[])
     KAlarmApp* app = KAlarmApp::create(argc, argv);
     QStringList args = app->arguments();
     app->setAttribute(Qt::AA_UseHighDpiPixmaps, true);
-#if QT_VERSION >= 0x050600
     app->setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
 
     KLocalizedString::setApplicationDomain("kalarm");
     KAboutData aboutData(QStringLiteral(PROGRAM_NAME), i18n("KAlarm"),
