34a8fb47cbc10c46afc7bc3e0ee1f7dbe6bd0aa8
David Jarvie <djarvie@kde.org>
2019-11-12 16:28:54 +0000

    Implement generic event models

---
+++ src/resources/eventmodel.cpp
@@ -0,0 +1,360 @@
+/*
+ *  eventmodel.cpp  -  model containing flat list of events
+ *  Program:  kalarm
+ *  Copyright © 2007-2019 David Jarvie <djarvie@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#include "eventmodel.h"
+
+#include "calendardatamodel.h"
+#include "messagebox.h"
+#include "preferences.h"
+#include "resources/resources.h"
+#include "kalarm_debug.h"
+
+#include <KLocalizedString>
+
+#include <QApplication>
+#include <QMouseEvent>
+#include <QHelpEvent>
+#include <QToolTip>
+
+/*============================================================================*/
+
+EventListModel::EventListModel(CalEvent::Types types, QObject* parent)
+    : QSortFilterProxyModel(parent)
+    , mAlarmTypes(types == CalEvent::EMPTY ? CalEvent::ACTIVE | CalEvent::ARCHIVED | CalEvent::TEMPLATE : types)
+{
+    setSourceModel(new KDescendantsProxyModel(this));
+    setSortRole(CalendarDataModel::SortRole);
+    setDynamicSortFilter(true);
+    connect(this, &QAbstractItemModel::rowsInserted, this, &EventListModel::slotRowsInserted);
+    connect(this, &QAbstractItemModel::rowsRemoved, this, &EventListModel::slotRowsRemoved);
+    connect(Resources::instance(), &Resources::resourcePopulated,
+                             this, &QSortFilterProxyModel::invalidate);
+    connect(Resources::instance(), &Resources::settingsChanged,
+                             this, &EventListModel::resourceSettingsChanged);
+}
+
+/******************************************************************************
+* Return the event in a specified row.
+*/
+KAEvent EventListModel::event(int row) const
+{
+    return event(index(row, 0));
+}
+
+/******************************************************************************
+* Return the event referred to by an index.
+*/
+KAEvent EventListModel::event(const QModelIndex& index) const
+{
+    KDescendantsProxyModel* proxyModel = static_cast<KDescendantsProxyModel*>(sourceModel());
+    const QModelIndex dataIndex = proxyModel->mapToSource(mapToSource(index));
+qDebug()<<"EventListModel::event:"<<dataIndex<<((*mEventFunction)(dataIndex)).id();
+    return (*mEventFunction)(dataIndex);
+}
+
+/******************************************************************************
+* Return the index to a specified event.
+*/
+QModelIndex EventListModel::eventIndex(const QString& eventId) const
+{
+    KDescendantsProxyModel* proxyModel = static_cast<KDescendantsProxyModel*>(sourceModel());
+qDebug()<<"EventListModel::eventIndex:"<<(*mEventIndexFunction)(eventId)<<" (should be AkonadiModel)";
+qDebug()<<"EventListModel::eventIndex:"<<mapFromSource(proxyModel->mapFromSource((*mEventIndexFunction)(eventId)))<<" (should be EventListModel)";
+    return mapFromSource(proxyModel->mapFromSource(((*mEventIndexFunction)(eventId))));
+}
+
+/******************************************************************************
+* Check whether the model contains any events.
+*/
+bool EventListModel::haveEvents() const
+{
+    return rowCount();
+}
+
+int EventListModel::iconWidth()
+{
+    return CalendarDataModel::iconSize().width();
+}
+
+bool EventListModel::hasChildren(const QModelIndex& parent) const
+{
+    return rowCount(parent) > 0;
+}
+
+bool EventListModel::canFetchMore(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return false;
+}
+
+QModelIndexList EventListModel::match(const QModelIndex& start, int role, const QVariant& value, int hits, Qt::MatchFlags flags) const
+{
+    if (role < Qt::UserRole)
+        return QSortFilterProxyModel::match(start, role, value, hits, flags);
+
+    QModelIndexList matches;
+    const QModelIndexList indexes = sourceModel()->match(mapToSource(start), role, value, hits, flags);
+    for (const QModelIndex& ix : indexes)
+    {
+        QModelIndex proxyIndex = mapFromSource(ix);
+        if (proxyIndex.isValid())
+            matches += proxyIndex;
+    }
+    return matches;
+}
+
+int EventListModel::columnCount(const QModelIndex& parent) const
+{
+    Q_UNUSED(parent);
+    return CalendarDataModel::ColumnCount;
+}
+
+QVariant EventListModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    return static_cast<KDescendantsProxyModel*>(sourceModel())->sourceModel()->headerData(section, orientation, role + mHeaderDataRoleOffset);
+}
+
+bool EventListModel::filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const
+{
+    // Get the resource which contains this event.
+    KDescendantsProxyModel* proxyModel = static_cast<KDescendantsProxyModel*>(sourceModel());
+    const QModelIndex dataIndex = proxyModel->mapToSource(proxyModel->index(sourceRow, 0, sourceParent));
+    const QString eventId = dataIndex.data(CalendarDataModel::EventIdRole).toString();
+    if (eventId.isEmpty())
+        return false;   // this row doesn't contain an event
+    const ResourceId id = dataIndex.data(CalendarDataModel::ParentResourceIdRole).toLongLong();
+    if (id < 0)
+        return false;   // the parent item isn't a resource
+    const Resource resource = Resources::resource(id);
+qDebug()<<"EventListModel::filterAcceptsRow 1:"<<(void*)this<<" resource ID:"<<id<<resource.displayName();
+    if (!resource.isValid())
+        return false;   // invalidly configured resource
+
+    // Get the event.
+    const KAEvent event = resource.event(eventId);
+    if (!event.isValid())
+        return false;
+qDebug()<<"EventListModel::filterAcceptsRow 2:"<<(void*)this<<" resource ID:"<<id<<"enabled:"<<resource.enabledTypes()<<", event:"<<event.category();
+    if (!(event.category() & mAlarmTypes))
+        return false;   // the event has the wrong alarm type
+    if (!resource.isEnabled(event.category()))
+        return false;   // the resource is disabled for this alarm type
+qDebug()<<"EventListModel::filterAcceptsRow YES:"<<(void*)this<<" event ID"<<eventId<<sourceModel();
+    return true;
+}
+
+bool EventListModel::filterAcceptsColumn(int sourceColumn, const QModelIndex& sourceParent) const
+{
+    if (sourceColumn >= CalendarDataModel::ColumnCount)
+        return false;
+    return QSortFilterProxyModel::filterAcceptsColumn(sourceColumn, sourceParent);
+}
+
+/******************************************************************************
+* Called when rows have been inserted into the model.
+*/
+void EventListModel::slotRowsInserted()
+{
+    if (!mHaveEvents  &&  rowCount())
+    {
+        mHaveEvents = true;
+        Q_EMIT haveEventsStatus(true);
+    }
+}
+
+/******************************************************************************
+* Called when rows have been deleted from the model.
+*/
+void EventListModel::slotRowsRemoved()
+{
+    if (mHaveEvents  &&  !rowCount())
+    {
+        mHaveEvents = false;
+        Q_EMIT haveEventsStatus(false);
+    }
+}
+
+/******************************************************************************
+* Called when a resource parameter or status has changed.
+* If the resource's enabled status has changed, re-filter the list to add or
+* remove its alarms.
+*/
+void EventListModel::resourceSettingsChanged(Resource& resource, ResourceType::Changes change)
+{
+    if (!resource.isValid())
+        return;
+    if (change == ResourceType::Enabled)
+    {
+        // Ensure that items for a newly enabled resource are always ordered
+        // correctly. Note that invalidateFilter() is not adequate for this.
+        invalidate();
+    }
+}
+
+
+/*=============================================================================
+= Class: AlarmListModel
+= Filter proxy model containing all alarms (not templates) of specified mime
+= types in enabled collections.
+=============================================================================*/
+AlarmListModel* AlarmListModel::mAllInstance = nullptr;
+
+AlarmListModel::AlarmListModel(QObject* parent)
+    : EventListModel(CalEvent::ACTIVE | CalEvent::ARCHIVED, parent)
+    , mFilterTypes(CalEvent::ACTIVE | CalEvent::ARCHIVED)
+{
+}
+
+AlarmListModel::~AlarmListModel()
+{
+    if (this == mAllInstance)
+        mAllInstance = nullptr;
+}
+
+void AlarmListModel::setEventTypeFilter(CalEvent::Types types)
+{
+    // Ensure that the filter isn't applied to the 'all' instance, and that
+    // 'types' doesn't include any alarm types not included in the model.
+    types &= alarmTypes();
+
+    if (this != mAllInstance
+    &&  types != mFilterTypes)
+    {
+        mFilterTypes = types;
+        invalidateFilter();
+    }
+}
+
+bool AlarmListModel::filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const
+{
+    if (!EventListModel::filterAcceptsRow(sourceRow, sourceParent))
+        return false;
+    if (mFilterTypes == CalEvent::EMPTY)
+        return false;
+    const int type = sourceModel()->data(sourceModel()->index(sourceRow, 0, sourceParent), CalendarDataModel::StatusRole).toInt();
+    return static_cast<CalEvent::Type>(type) & mFilterTypes;
+}
+
+bool AlarmListModel::filterAcceptsColumn(int sourceCol, const QModelIndex& ix) const
+{
+    if (!EventListModel::filterAcceptsColumn(sourceCol, ix))
+        return false;
+    return (sourceCol != CalendarDataModel::TemplateNameColumn);
+}
+
+QVariant AlarmListModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Horizontal)
+    {
+        if (section < 0  ||  section >= ColumnCount)
+            return QVariant();
+    }
+    return EventListModel::headerData(section, orientation, role);
+}
+
+
+/*=============================================================================
+= Class: TemplateListModel
+= Filter proxy model containing all alarm templates for specified alarm types
+= in enabled resources.
+=============================================================================*/
+TemplateListModel* TemplateListModel::mAllInstance = nullptr;
+
+TemplateListModel::TemplateListModel(QObject* parent)
+    : EventListModel(CalEvent::TEMPLATE, parent)
+    , mActionsEnabled(KAEvent::ACT_ALL)
+    , mActionsFilter(KAEvent::ACT_ALL)
+{
+}
+
+TemplateListModel::~TemplateListModel()
+{
+    if (this == mAllInstance)
+        mAllInstance = nullptr;
+}
+
+void TemplateListModel::setAlarmActionFilter(KAEvent::Actions types)
+{
+    // Ensure that the filter isn't applied to the 'all' instance.
+    if (this != mAllInstance  &&  types != mActionsFilter)
+    {
+        mActionsFilter = types;
+        invalidateFilter();
+    }
+}
+
+void TemplateListModel::setAlarmActionsEnabled(KAEvent::Actions types)
+{
+    // Ensure that the setting isn't applied to the 'all' instance.
+    if (this != mAllInstance  &&  types != mActionsEnabled)
+    {
+        mActionsEnabled = types;
+        invalidateFilter();
+    }
+}
+
+bool TemplateListModel::filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const
+{
+    if (!EventListModel::filterAcceptsRow(sourceRow, sourceParent))
+        return false;
+    if (mActionsFilter == KAEvent::ACT_ALL)
+        return true;
+    const QModelIndex sourceIndex = sourceModel()->index(sourceRow, 0, sourceParent);
+    const KAEvent::Actions actions = static_cast<KAEvent::Actions>(sourceModel()->data(sourceIndex, CalendarDataModel::AlarmActionsRole).toInt());
+    return actions & mActionsFilter;
+}
+
+bool TemplateListModel::filterAcceptsColumn(int sourceCol, const QModelIndex&) const
+{
+    return sourceCol == CalendarDataModel::TemplateNameColumn
+       ||  sourceCol == CalendarDataModel::TypeColumn;
+}
+
+QVariant TemplateListModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Horizontal)
+    {
+        switch (section)
+        {
+            case TypeColumn:
+                section = CalendarDataModel::TypeColumn;
+                break;
+            case TemplateNameColumn:
+                section = CalendarDataModel::TemplateNameColumn;
+                break;
+            default:
+                return QVariant();
+        }
+    }
+    return EventListModel::headerData(section, orientation, role);
+}
+
+Qt::ItemFlags TemplateListModel::flags(const QModelIndex& index) const
+{
+    Qt::ItemFlags f = sourceModel()->flags(mapToSource(index));
+    if (mActionsEnabled == KAEvent::ACT_ALL)
+        return f;
+    const KAEvent::Actions actions = static_cast<KAEvent::Actions>(EventListModel::data(index, CalendarDataModel::AlarmActionsRole).toInt());
+    if (!(actions & mActionsEnabled))
+        f = static_cast<Qt::ItemFlags>(f & ~(Qt::ItemIsEnabled | Qt::ItemIsSelectable));
+    return f;
+}
+
+// vim: et sw=4:
---
+++ src/resources/eventmodel.h
@@ -0,0 +1,275 @@
+/*
+ *  eventmodel.h  -  model containing flat list of events
+ *  Program:  kalarm
+ *  Copyright © 2010-2019 David Jarvie <djarvie@kde.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef EVENTMODEL_H
+#define EVENTMODEL_H
+
+#include "resource.h"
+
+#include <kalarmcal/kacalendar.h>
+
+#include <KDescendantsProxyModel>
+
+#include <QSortFilterProxyModel>
+
+using namespace KAlarmCal;
+
+/*=============================================================================
+= Class: EventListModel
+= Proxy model to filter a resource data model to restrict its contents to
+= events, not resources, containing specified alarm types in enabled resources.
+=============================================================================*/
+class EventListModel : public QSortFilterProxyModel
+{
+    Q_OBJECT
+public:
+    /** Constructs a new instance.
+     *  @param  types      The alarm types (active/archived/template) included in this model
+     *  @param  parent     The parent object
+     *  @tparam DataModel  The data model class to use as the source model. It must
+     *                     have the following methods:
+     *                       static Model* instance(); - returns the unique instance.
+     *                       KAEvent     event(const QModelIndex&) const;
+     *                       QModelIndex eventIndex(const QString&) const;
+     *                       int         headerDataEventRoleOffset() const;
+     */
+    template <class DataModel>
+    static EventListModel* create(CalEvent::Types types, QObject* parent = nullptr);
+
+    /** Return the alarm types included in the model. */
+    CalEvent::Types alarmTypes() const   { return mAlarmTypes; }
+
+    KAEvent      event(int row) const;
+    KAEvent      event(const QModelIndex&) const;
+    using QObject::event;   // prevent warning about hidden virtual method
+    QModelIndex  eventIndex(const QString& eventId) const;
+
+    /** Determine whether the model contains any items. */
+    bool haveEvents() const;
+
+    static int iconWidth();
+
+    bool hasChildren(const QModelIndex &parent = QModelIndex()) const override;
+    bool canFetchMore(const QModelIndex &parent) const override;
+    QModelIndexList match(const QModelIndex &start, int role, const QVariant &value, int hits = 1, Qt::MatchFlags flags = Qt::MatchFlags(Qt::MatchStartsWith | Qt::MatchWrap)) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+Q_SIGNALS:
+    /** Signal emitted when either the first item is added to the model,
+     *  or when the last item is deleted from the model.
+     */
+    void haveEventsStatus(bool have);
+
+protected:
+    /** Constructor. Note that initialise() must be called to complete the construction. */
+    EventListModel(CalEvent::Types types, QObject* parent);
+
+    /** To be called after construction as a base class.
+     *  @tparam DataModel  The data model class to use as the source model. It must
+     *                     have the following methods:
+     *                       static Model* instance(); - returns the unique instance.
+     *                       KAEvent     event(const QModelIndex&) const;
+     *                       QModelIndex eventIndex(const QString&) const;
+     *                       int         headerDataEventRoleOffset() const;
+     */
+    template <class DataModel> void initialise();
+
+    bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const override;
+    bool filterAcceptsColumn(int sourceColumn, const QModelIndex &sourceParent) const override;
+
+private Q_SLOTS:
+    void slotRowsInserted();
+    void slotRowsRemoved();
+    void resourceSettingsChanged(Resource&, ResourceType::Changes);
+
+private:
+    KAEvent      (*mEventFunction)(const QModelIndex&) {nullptr};  // function to fetch event from data model
+    QModelIndex  (*mEventIndexFunction)(const QString&) {nullptr};  // function to fetch event index from data model
+    CalEvent::Types mAlarmTypes{CalEvent::EMPTY};  // only include events with these alarm types
+    int             mHeaderDataRoleOffset{0};  // offset for base class to add to headerData() role
+    bool            mHaveEvents{false};        // there are events in this model
+};
+
+
+/*=============================================================================
+= Class: AlarmListModel
+= Filter proxy model containing all alarms of specified alarm types in enabled
+= resources.
+=============================================================================*/
+class AlarmListModel : public EventListModel
+{
+    Q_OBJECT
+public:
+    enum   // data columns
+    {
+        TimeColumn = 0, TimeToColumn, RepeatColumn, ColourColumn, TypeColumn, TextColumn,
+        ColumnCount
+    };
+
+    template <class DataModel>
+    static AlarmListModel* create(QObject* parent = nullptr);
+
+    ~AlarmListModel();
+
+    /** Return the model containing all active and archived alarms. */
+    template <class DataModel>
+    static AlarmListModel* all();
+
+    /** Set a filter to restrict the event types to a subset of those
+     *  specified in the constructor.
+     *  @param types the event types to be included in the model
+     */
+    void setEventTypeFilter(CalEvent::Types types);
+
+    /** Return the filter set by setEventTypeFilter().
+     *  @return all event types included in the model
+     */
+    CalEvent::Types eventTypeFilter() const   { return mFilterTypes; }
+
+    int  columnCount(const QModelIndex& = QModelIndex()) const  override { return ColumnCount; }
+    QVariant headerData(int section, Qt::Orientation, int role = Qt::DisplayRole) const override;
+
+protected:
+    explicit AlarmListModel(QObject* parent);
+
+    bool filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const override;
+    bool filterAcceptsColumn(int sourceCol, const QModelIndex& sourceParent) const override;
+
+private:
+    static AlarmListModel* mAllInstance;
+    CalEvent::Types mFilterTypes;  // types of events contained in this model
+};
+
+
+/*=============================================================================
+= Class: TemplateListModel
+= Filter proxy model containing all alarm templates for specified alarm types
+= in enabled resources.
+=============================================================================*/
+class TemplateListModel : public EventListModel
+{
+    Q_OBJECT
+public:
+    enum {   // data columns
+        TypeColumn, TemplateNameColumn,
+        ColumnCount
+    };
+
+    template <class DataModel>
+    static TemplateListModel* create(QObject* parent = nullptr);
+
+    ~TemplateListModel();
+
+    /** Return the model containing all alarm templates. */
+    template <class DataModel>
+    static TemplateListModel* all();
+
+    /** Set which alarm action types should be included in the model. */
+    void setAlarmActionFilter(KAEvent::Actions);
+
+    /** Return which alarm action types are included in the model. */
+    KAEvent::Actions alarmActionFilter() const  { return mActionsFilter; }
+
+    /** Set which alarm types should be shown as disabled in the model. */
+    void setAlarmActionsEnabled(KAEvent::Actions);
+
+    /** Set which alarm types should be shown as disabled in the model. */
+    KAEvent::Actions setAlarmActionsEnabled() const  { return mActionsEnabled; }
+
+    int  columnCount(const QModelIndex& = QModelIndex()) const override  { return ColumnCount; }
+    QVariant headerData(int section, Qt::Orientation, int role = Qt::DisplayRole) const override;
+    Qt::ItemFlags flags(const QModelIndex&) const override;
+
+protected:
+    explicit TemplateListModel(QObject* parent);
+
+    bool filterAcceptsRow(int sourceRow, const QModelIndex& sourceParent) const override;
+    bool filterAcceptsColumn(int sourceCol, const QModelIndex& sourceParent) const override;
+
+private:
+    static TemplateListModel* mAllInstance;
+
+    KAEvent::Actions mActionsEnabled;  // disable types not in this mask
+    KAEvent::Actions mActionsFilter;   // hide types not in this mask
+};
+
+
+/*=============================================================================
+* Template definitions.
+*============================================================================*/
+
+template <class DataModel>
+EventListModel* EventListModel::create(CalEvent::Types types, QObject* parent)
+{
+    EventListModel* model = new EventListModel(types, parent);
+    model->initialise<DataModel>();
+    return model;
+}
+
+template <class DataModel> void EventListModel::initialise()
+{
+    static_cast<KDescendantsProxyModel*>(sourceModel())->setSourceModel(DataModel::instance());
+    mHeaderDataRoleOffset = DataModel::instance()->headerDataEventRoleOffset();
+    mEventFunction      = [](const QModelIndex& ix) { return DataModel::instance()->event(ix); };
+    mEventIndexFunction = [](const QString& id) { return DataModel::instance()->eventIndex(id); };
+}
+
+template <class DataModel>
+AlarmListModel* AlarmListModel::create(QObject* parent)
+{
+    AlarmListModel* model = new AlarmListModel(parent);
+    model->EventListModel::initialise<DataModel>();
+    return model;
+}
+
+template <class DataModel>
+AlarmListModel* AlarmListModel::all()
+{
+    if (!mAllInstance)
+    {
+        mAllInstance = create<DataModel>(DataModel::instance());
+        mAllInstance->sort(TimeColumn, Qt::AscendingOrder);
+    }
+    return mAllInstance;
+}
+
+template <class DataModel>
+TemplateListModel* TemplateListModel::create(QObject* parent)
+{
+    TemplateListModel* model = new TemplateListModel(parent);
+    model->EventListModel::initialise<DataModel>();
+    return model;
+}
+
+template <class DataModel>
+TemplateListModel* TemplateListModel::all()
+{
+    if (!mAllInstance)
+    {
+        mAllInstance = create<DataModel>(DataModel::instance());
+        mAllInstance->sort(TemplateNameColumn, Qt::AscendingOrder);
+    }
+    return mAllInstance;
+}
+
+#endif // EVENTMODEL_H
+
+// vim: et sw=4:
