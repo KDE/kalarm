1e83c544165739ce8b83be0f6060e624297327e6
Christophe Giboudeaux <christophe@krop.fr>
2020-08-13 17:12:24 +0200

    Convert license headers to SPDX format

--- src/templatedlg.h
+++ src/templatedlg.h
@@ -1,21 +1,9 @@
 /*
  *  templatedlg.h  -  dialog to create, edit and delete alarm templates
  *  Program:  kalarm
- *  Copyright © 2004,2006,2007,2010 by David Jarvie <djarvie@kde.org>
+ *  SPDX-FileCopyrightText: 2004, 2006, 2007, 2010 David Jarvie <djarvie@kde.org>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  SPDX-License-Identifier: GPL-2.0-or-later
  */
 #ifndef TEMPLATEDLG_H
 #define TEMPLATEDLG_H
