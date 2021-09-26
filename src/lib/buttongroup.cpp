82369401d6e02e69946d5bbcf3d19c7f075d90af
Laurent Montel <montel@kde.org>
2021-09-26 08:41:51 +0200

    Not necessary to use qOverload here

--- src/lib/buttongroup.cpp
+++ src/lib/buttongroup.cpp
@@ -14,7 +14,7 @@
 ButtonGroup::ButtonGroup(QObject* parent)
     : QButtonGroup(parent)
 {
-    connect(this, qOverload<QAbstractButton*>(&QButtonGroup::buttonClicked), this, &ButtonGroup::buttonSet);
+    connect(this, &QButtonGroup::buttonClicked, this, &ButtonGroup::buttonSet);
 }
 
 /******************************************************************************
